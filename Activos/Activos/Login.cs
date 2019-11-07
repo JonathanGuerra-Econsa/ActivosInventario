b6a3c456dd6baeac91d24da1b3ccf7c370d8a43f
jguerra <jguerra.grupoeconsa@gmail.com>
2019-11-07 08:12:50 -0600

    Login

---
+++ Activos/Activos/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Activos
+{
+    public partial class Login : Form
+    {
+        ConsultasMySQL_JG consultasMySQL = new ConsultasMySQL_JG();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Departamento de Informática 2020", "Creditos", MessageBoxButtons.OK);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
