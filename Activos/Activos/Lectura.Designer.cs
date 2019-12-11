cb8450565665337325a9ef5a0078f25fc8517589
jguerra <jguerra.grupoeconsa@gmail.com>
2019-12-11 07:26:56 -0600

    Lectura begins

--- Activos/Activos/Lectura.Designer.cs
+++ Activos/Activos/Lectura.Designer.cs
@@ -30,10 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lectura));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtEscaner = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSoporte = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbTituloDescripcion = new System.Windows.Forms.Label();
+            this.lbDescripcion = new System.Windows.Forms.Label();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.lbTituloUsuario = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -41,69 +46,127 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(81, 27);
+            this.label1.Location = new System.Drawing.Point(161, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(295, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Lectura de Etiquetas";
             // 
-            // textBox1
+            // txtEscaner
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(55, 109);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(339, 35);
-            this.textBox1.TabIndex = 1;
+            this.txtEscaner.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEscaner.Location = new System.Drawing.Point(135, 110);
+            this.txtEscaner.MaxLength = 8;
+            this.txtEscaner.Name = "txtEscaner";
+            this.txtEscaner.Size = new System.Drawing.Size(339, 35);
+            this.txtEscaner.TabIndex = 1;
+            this.txtEscaner.TextChanged += new System.EventHandler(this.txtEscaner_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(164, 81);
+            this.label2.Location = new System.Drawing.Point(244, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Escanee Aquí";
             // 
-            // panel1
+            // btnSoporte
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 159);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(433, 166);
-            this.panel1.TabIndex = 3;
+            this.btnSoporte.BackColor = System.Drawing.Color.Transparent;
+            this.btnSoporte.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSoporte.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoporte.Image = global::Activos.Properties.Resources.customer_support;
+            this.btnSoporte.Location = new System.Drawing.Point(210, 371);
+            this.btnSoporte.Name = "btnSoporte";
+            this.btnSoporte.Size = new System.Drawing.Size(172, 82);
+            this.btnSoporte.TabIndex = 4;
+            this.btnSoporte.Text = "Dar soporte";
+            this.btnSoporte.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSoporte.UseVisualStyleBackColor = false;
+            this.btnSoporte.Click += new System.EventHandler(this.btnSoporte_Click);
             // 
-            // button1
+            // groupBox1
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = global::Activos.Properties.Resources.customer_support;
-            this.button1.Location = new System.Drawing.Point(132, 331);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 82);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Dar soporte";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button1.UseVisualStyleBackColor = false;
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.lbUsuario);
+            this.groupBox1.Controls.Add(this.lbTituloUsuario);
+            this.groupBox1.Controls.Add(this.lbDescripcion);
+            this.groupBox1.Controls.Add(this.lbTituloDescripcion);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(578, 214);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detalle";
+            // 
+            // lbTituloDescripcion
+            // 
+            this.lbTituloDescripcion.AutoSize = true;
+            this.lbTituloDescripcion.BackColor = System.Drawing.Color.Transparent;
+            this.lbTituloDescripcion.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTituloDescripcion.Location = new System.Drawing.Point(24, 33);
+            this.lbTituloDescripcion.Name = "lbTituloDescripcion";
+            this.lbTituloDescripcion.Size = new System.Drawing.Size(80, 17);
+            this.lbTituloDescripcion.TabIndex = 6;
+            this.lbTituloDescripcion.Text = "Descripción:";
+            // 
+            // lbDescripcion
+            // 
+            this.lbDescripcion.AutoSize = true;
+            this.lbDescripcion.BackColor = System.Drawing.Color.Transparent;
+            this.lbDescripcion.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescripcion.Location = new System.Drawing.Point(22, 50);
+            this.lbDescripcion.Name = "lbDescripcion";
+            this.lbDescripcion.Size = new System.Drawing.Size(112, 25);
+            this.lbDescripcion.TabIndex = 6;
+            this.lbDescripcion.Text = "Descripción";
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lbUsuario.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsuario.Location = new System.Drawing.Point(22, 113);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(77, 25);
+            this.lbUsuario.TabIndex = 7;
+            this.lbUsuario.Text = "Usuario";
+            // 
+            // lbTituloUsuario
+            // 
+            this.lbTituloUsuario.AutoSize = true;
+            this.lbTituloUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lbTituloUsuario.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTituloUsuario.Location = new System.Drawing.Point(24, 96);
+            this.lbTituloUsuario.Name = "lbTituloUsuario";
+            this.lbTituloUsuario.Size = new System.Drawing.Size(57, 17);
+            this.lbTituloUsuario.TabIndex = 8;
+            this.lbTituloUsuario.Text = "Usuario:";
             // 
             // Lectura
             // 
+            this.AcceptButton = this.btnSoporte;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Activos.Properties.Resources.FONDO_4;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(457, 447);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(602, 488);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSoporte);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtEscaner);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Lectura";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lectura";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,9 +175,13 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtEscaner;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSoporte;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.Label lbTituloUsuario;
+        private System.Windows.Forms.Label lbDescripcion;
+        private System.Windows.Forms.Label lbTituloDescripcion;
     }
 }
